34ea84cf63f33df1797de19a48408eccd52210e4
Sergio Augusto Lordano Luiz <sergio.lordano@lnls379-linux.abtlus.org.br>
2020-03-21 16:51:34 -0300

    added requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="optlnls", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Sergio Lordano",
     author_email="sergiolordano2@gmail.com",
     description="X-ray optics utilities",
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SergioLordano/optlnls",
     packages=setuptools.find_packages(),
+	install_requires=[
+		"numpy", "matplotlib", "scipy"
+	]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
