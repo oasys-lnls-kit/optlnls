54a214d8353775867cc96a34d657eb2ebfd1adce
Sergio Augusto Lordano Luiz <sergio.lordano@lnls379-linux.abtlus.org.br>
2020-03-21 12:55:15 -0300

    changed folder tree

---
+++ optlnls/crystal.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 12:47:43 2020
+
+@author: sergio.lordano
+"""
+
---
+++ optlnls/mirror.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 12:46:58 2020
+
+@author: sergio.lordano
+"""
+
