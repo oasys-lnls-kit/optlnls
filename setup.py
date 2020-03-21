4b7fac3940ebdfbf2fa9be76f607500daed80d9c
Sergio Augusto Lordano Luiz <sergio.lordano@lnls379-linux.abtlus.org.br>
2020-03-21 16:55:13 -0300

    fixing setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
 	install_requires=[
 		"numpy", "matplotlib", "scipy"
-	]
+	],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
