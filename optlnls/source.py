2d3a818a7ee5b948db77611aec6313f5d855abe6
Sergio Augusto Lordano Luiz <sergio.lordano@lnls379-linux.abtlus.org.br>
2020-05-05 16:33:32 -0300

    added srw function to read external field

--- optlnls/source.py
+++ optlnls/source.py
@@ -124,7 +124,43 @@ def srw_undulator_spectrum(mag_field=[], electron_beam=[], energy_grid=[], sampl
 
 
 
-
+def AuxReadInMagFld3D(filePath, sCom):
+    
+    """Function from SRW examples"""
+    
+    from array import array
+    from srwlib import SRWLMagFld3D
+    
+    f = open(filePath, 'r')
+    f.readline() #1st line: just pass
+    global xStart,xStep,xNp,yStart,yStep,yNp,zStart,zStep,zNp
+    xStart = float(f.readline().split(sCom, 2)[1]) #2nd line: initial X position [m]; it will not actually be used
+    xStep = float(f.readline().split(sCom, 2)[1]) #3rd line: step vs X [m]
+    xNp = int(f.readline().split(sCom, 2)[1]) #4th line: number of points vs X
+    yStart = float(f.readline().split(sCom, 2)[1]) #5th line: initial Y position [m]; it will not actually be used
+    yStep = float(f.readline().split(sCom, 2)[1]) #6th line: step vs Y [m]
+    yNp = int(f.readline().split(sCom, 2)[1]) #7th line: number of points vs Y
+    zStart = float(f.readline().split(sCom, 2)[1]) #8th line: initial Z position [m]; it will not actually be used
+    zStep = float(f.readline().split(sCom, 2)[1]) #9th line: step vs Z [m]
+    zNp = int(f.readline().split(sCom, 2)[1]) #10th line: number of points vs Z
+    totNp = xNp*yNp*zNp
+    locArBx = array('d', [0]*totNp)
+    locArBy = array('d', [0]*totNp)
+    locArBz = array('d', [0]*totNp)
+    for i in range(totNp):
+        curLineParts = f.readline().split('\t')
+        locArBx[i] = float(curLineParts[0])
+        locArBy[i] = float(curLineParts[1])
+        locArBz[i] = float(curLineParts[2])
+    f.close()
+    xRange = xStep
+    if xNp > 1: xRange = (xNp - 1)*xStep
+    yRange = yStep
+    if yNp > 1: yRange = (yNp - 1)*yStep
+    zRange = zStep
+    if zNp > 1: zRange = (zNp - 1)*zStep
+        
+    return SRWLMagFld3D(locArBx, locArBy, locArBz, xNp, yNp, zNp, xRange, yRange, zRange, 1)
 
 
 
