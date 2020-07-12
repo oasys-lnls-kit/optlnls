da4ba54655b963c5ed1c8658fd1df3368f43fb09
SergioLordano <sergiolordano2@gmail.com>
2020-07-12 16:14:10 -0300

    added constants module

---
+++ optlnls/constants.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 11:10:11 2020
+
+@author: lordano
+
+data from https://physics.nist.gov/cuu/Constants/index.html
+"""
+
+
+
+pi = 3.141592653589793
+
+electron_charge = 1.602176634e-19 # Coulomb
+
+planck = 6.62607015e-34 # J.s = m * m . kg / s
+
+planck_reduced = planck / (2 * pi)
+
+speed_of_light = 2.99792458e8 # m/s
+
+hc = planck / electron_charge * speed_of_light # eV.m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
