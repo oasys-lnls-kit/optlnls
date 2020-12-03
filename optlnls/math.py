4dc13d140962bbeb7abdf459b007588635f6268f
Sergio Augusto Lordano Luiz <sergio.lordano@lnls379-linux.abtlus.org.br>
2020-12-03 18:06:44 -0300

    added function to bin matrix

--- optlnls/math.py
+++ optlnls/math.py
@@ -93,6 +93,46 @@ def derivate_keeping_size(x, y):
     return np.array(der)
 
 
+def bin_matrix(matrix, binning_y, binning_x):
+
+    yn, xn = matrix.shape
+
+    if ((xn % binning_x != 0) or (yn % binning_y != 0)):
+        print('array of shape ({0} x {1}) cannot be binned by factor ({2},{3})'.format(yn, xn, binning_y, binning_x))
+        return matrix
+    
+    else:
+        print('binning matrix of shape({0},{1}) by factors ({2},{3})'.format(yn, xn, binning_y, binning_x))
+        xn = int(xn / binning_x)
+        yn = int(yn / binning_y)
+            
+        matrix_binned = np.zeros((yn,xn), dtype=float)
+        
+        count_y = 0
+        for iy in range(yn):
+    
+            count_x = 0
+            for ix in range(xn):
+                
+                matrix_binned[iy,ix] = np.sum(matrix[count_y:count_y+binning_y,
+                                                     count_x:count_x+binning_x])
+    
+                count_x += binning_x
+            count_y += binning_y
+            
+        matrix_binned /= binning_x*binning_y
+      
+        if(0):
+            fig, ax = plt.subplots(figsize=(12,4), ncols=2)
+            im0 = ax[0].imshow(matrix, origin='lower')
+            im1 = ax[1].imshow(matrix_binned, origin='lower')
+            fig.colorbar(im0, ax=ax[0])
+            fig.colorbar(im1, ax=ax[1])
+        
+            plt.show()
+     
+        return matrix_binned
+
 
 
 def get_fwhm(x, y, oversampling=1, zero_padding=True, avg_correction=False, 
