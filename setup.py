9efea0d15903fce0dd3688b2f1996289a34e14ad
giovanni.ribeiro <giovanni.ribeiro@lnls.br>
2024-07-03 14:59:22 -0300

    corrected version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="optlnls",
-    version="0.3.16",
+    version="0.3.17",
     author="Sergio Lordano",
     author_email="sergiolordano2@gmail.com",
     description="X-ray optics utilities",
